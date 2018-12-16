e79a88adeb1984c2fa7dc9bd303de9862885fda8
oskll <kocaozogun@gmail.com>
2018-12-16 01:58:35 +0100

    Ajout de l'interface de connexion, encore a finir pour la partie créer un compte

---
+++ ApplicationReseau/src/View/Main.java
@@ -0,0 +1,32 @@
+package View;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+		
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("gui/Sample.fxml"));
+			primaryStage.initStyle(StageStyle.UNDECORATED);
+			Scene scene = new Scene(root,843,476);
+			scene.getStylesheets().add(getClass().getResource("gui/application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
