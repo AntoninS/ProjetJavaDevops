a55fbdaa41f69b4783ba0c36c8a04a56b593db2a
Cedric Peyronnet <maxi072@laposte.net>
2019-01-15 09:27:01 +0100

    Ajout d'une course et du gestionnaire

---
+++ ApplicationReseau/src/Model/common/course/CourseGeneral.java
@@ -0,0 +1,25 @@
+package Model.common.course;
+
+import Model.common.Cheval;
+import Model.common.course.Course;
+
+import java.util.ArrayList;
+
+public class CourseGeneral extends Course
+{
+
+    public CourseGeneral()
+    {
+
+    }
+
+    @Override
+    protected void lancerCourse()
+    {
+        //Recuperation de la liste de chevaux
+
+        //Faire une boucle pour le lancement de la course
+
+        //Affichage de la progression des chevaux
+    }
+}
