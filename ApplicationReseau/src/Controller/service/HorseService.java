a796bbd0ed7d1045ea6f1545c8907efd69b7cd56
Antonin SPONTAK <antonin.spontak@gmail.com>
2019-01-29 11:22:27 +0100

    feat: Recupération liste chevaux depuis BDD et implémentation méthode update monay

---
+++ ApplicationReseau/src/Controller/service/HorseService.java
@@ -0,0 +1,69 @@
+package Controller.service;
+
+import Model.common.Cheval;
+import Model.common.User;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class HorseService {
+
+    DataBaseService dbs = DataBaseService.getInstance();
+
+    private static HorseService horseServiceInstance = null;
+
+    private static final int NUMBER_HORSES_COURSE = 6;
+
+    public HorseService() {
+
+    }
+
+    public static HorseService getInstance() {
+        if (horseServiceInstance == null) {
+            horseServiceInstance = new HorseService();
+        }
+        return horseServiceInstance;
+    }
+
+
+    public List<Cheval> getListHorsesFull() {
+        List<Cheval> list = new ArrayList<>();
+        try {
+            Connection con = dbs.getDataBaseConnexion();
+            Statement cs = null;
+            ResultSet rs = null;
+
+            String request = "SELECT ID, NAME, SPEED, SHAPE FROM PJ_HORSE";
+            cs = con.createStatement();
+            rs = cs.executeQuery(request);
+
+            while(rs.next()) {
+                list.add(new Cheval(rs.getString("NAME"),
+                        rs.getInt("SHAPE"),
+                        rs.getInt("ID"),
+                        0.0,
+                        rs.getDouble("SPEED")));
+            }
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public List<Cheval> getListHorsesLimitAndRandom() {
+        List<Cheval> list = getListHorsesFull();
+        if (null != list && !list.isEmpty() && list.size() >= NUMBER_HORSES_COURSE) {
+            Collections.shuffle(list);
+            return new ArrayList<>(list.subList(0, NUMBER_HORSES_COURSE));
+        }
+        return list;
+    }
+}
