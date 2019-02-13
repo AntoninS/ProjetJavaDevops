d6934b31b1b3a0d1f4ab9dd4e22f7824f2eca87b
Antonin SPONTAK <antonin.spontak@gmail.com>
2019-02-13 15:11:11 +0100

    fix: Possibilité de parier de nouveau après une course et number only

--- ApplicationReseau/src/Controller/client/MainClient.java
+++ ApplicationReseau/src/Controller/client/MainClient.java
@@ -8,25 +8,24 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
-public class MainClient extends Application{
+public class MainClient extends Application {
 
-	public static void main(String[] args) throws UnknownHostException, IOException {
-			
-			MainClient.launch(Authentication.class);
+    public static void main(String[] args) throws UnknownHostException, IOException {
 
-	}
-	
-	private static void printUsage()
-	{
-		System.out.println("java Model.Client.Client<address> <port>");
-		System.out.println("\t<address>: Controller.server's ip address");
-		System.out.println("\t<port>: Controller.server's port");
-	}
+        MainClient.launch(Authentication.class);
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+    }
+
+    private static void printUsage() {
+        System.out.println("java Model.Client.Client<address> <port>");
+        System.out.println("\t<address>: Controller.server's ip address");
+        System.out.println("\t<port>: Controller.server's port");
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
 
 }
